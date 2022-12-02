56cd08a649ebb831725abbe728f9ede557595705
slycatfirst <nikita-vorotyncev@yandex.ru>
2022-12-02 15:57:20 +0300

    add swiper

---
+++ swiper/slider.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".swiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    slidesPerView: 3
+  });
